ca3493537d40740ff71a78554609ec2916aa1338
Raheem Kawojue <kawojue08@gmail.com>
2022-12-09 21:50:07 +0100

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 function App() {
 
   return (
-    <>
-      <p>Greetings - Hello, World!</p>
-    </>
+    <main>
+      <form>
+        <div className="form-group">
+          <label>Card Number</label>
+          <input type="text" />
+        </div>
+        <article className="expr-cvv">
+          <div className="form-group">
+            <label>MM / YY</label>
+            <input type="text" maxLength={7} />
+          </div>
+          <div className="form-group">
+            <label>CVV</label>
+            <input type="text" />
+          </div>
+        </article>
+        <button type="submit" disabled={false}
+          className="btn focus:outline-none active:scale-105 hover:bg-blue-400 disabled:bg-[lightblue]">
+          pay
+        </button>
+      </form>
+    </main>
   )
 }
 
