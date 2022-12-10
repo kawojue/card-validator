b4aaa487ebd20bfcb22c418c6cbac330ff125f9f
Raheem Kawojue <kawojue08@gmail.com>
2022-12-10 01:57:06 +0100

    cardholder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import validator from "validator"
+import Modal from "./Modal"
 import reducer from "./reducer"
 import { useState, useReducer, useEffect } from 'react'
 
@@ -13,11 +13,18 @@ function App() {
   const [state, dispatch] = useReducer(reducer, defaultStates)
 
   const [cvc, setCVC] = useState('')
-  const [cardNumber, setCardNumber] = useState('')
   const [exprDate, setExprDate] = useState('')
+  const [cardNumber, setCardNumber] = useState('')
+  const [cardholderName, setCardholderName] = useState('')
 
-  const isValid = (cvc, cardNumber, exprDate) => {
-    if ((cvc && cardNumber && exprDate) && (!cvc.match(/[a-z]/i) && !cardNumber.match(/[a-z]/i)) && (cvc.length === 3 && cardNumber.length === 16)) {
+
+  const isValid = (cvc, cardNumber, exprDate, cardholderName) => {
+    if ((cvc && cardNumber
+      && exprDate && cardholderName)
+      && (!cvc.match(/[a-z]/i)
+        && !cardNumber.match(/[a-z]/i))
+      && ((cvc.length === 3 || cvc.length === 4)
+        && cardNumber.length === 16)) {
       return true
     }
     return false
@@ -31,33 +38,48 @@ function App() {
     setExprDate(output)
   }
 
-  const handleCardNumber = values => {
-    const splitted = values.match(/.{1,4}/g) || []
-    let output = ''
-    splitted.forEach(split => {
-      output += split
-    })
-    setCardNumber(output)
-  }
-
-  const handleModal = (cardNumber, cvc) => {
+  const handleModal = (cardNumber, cvc, cardholderName) => {
     if (cardNumber.match(/[a-z]/i) || cvc.match(/[a-z]/i)) {
-      dispatch({ type: 'CN-ERROR' })
+      dispatch({ type: 'STR-ERR' })
+    }
+
+    if (cvc.length > 4) {
+      dispatch({ type: 'LEN-ERR' })
+    }
+
+    if (cardholderName.match(/\d/)) {
+      dispatch({ type: 'NAME-ERR' })
     }
   }
 
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE' })
+  }
+
   useEffect(() => {
-    handleModal(cardNumber, cvc)
-  }, [cardNumber, cvc])
+    handleModal(cardNumber, cvc, cardholderName)
+  }, [cardNumber, cvc, cardholderName])
+
+  const pay = () => {
+    const newInfo = { cardNumber, cvc, cardholderName, exprDate }
+    dispatch({ type: 'PAY', payload: newInfo })
+
+    setCVC('')
+    setExprDate('')
+    setCardNumber('')
+    setCardholderName('')
+  }
 
   return (
     <main>
       <form onSubmit={e => e.preventDefault()}>
-        {state.msg && <p>{state.msgContent}</p>}
+        <div className="w-full h-5 mb-3">
+          {state.msg && <Modal close={closeModal} msg={state.msgContent} />}
+        </div>
         <div>
           <label>Card Number</label>
-          <input type="text" value={cardNumber}
-            onChange={(e) => handleCardNumber(e.target.value)} />
+          <input type="text" value={cardNumber} maxLength={16}
+            onChange={(e) => setCardNumber(e.target.value)} />
         </div>
         <article className="expr-cvv">
           <div>
@@ -67,12 +89,18 @@ function App() {
           </div>
           <div>
             <label>CVC</label>
-            <input type="text" value={cvc} maxLength={3}
+            <input type="text" value={cvc} maxLength={5}
               onChange={(e) => setCVC(e.target.value)} />
           </div>
         </article>
-        <button type="submit" disabled={!isValid(cvc, cardNumber, exprDate)}
-          className="btn focus:outline-none active:scale-105 hover:bg-blue-400 disabled:bg-[lightblue]">
+        <div className="mt-5">
+          <label>Cardholder Name</label>
+          <input type="text" value={cardholderName}
+            onChange={(e) => setCardholderName(e.target.value)} />
+        </div>
+        <button type="submit" disabled={!isValid(cvc, cardNumber, exprDate, cardholderName)}
+          onClick={() => pay()}
+          className="btn focus:outline-none active:scale-[1.25] hover:bg-blue-400 disabled:bg-[lightblue]">
           pay
         </button>
       </form>
