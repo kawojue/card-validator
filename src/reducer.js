b85224bad53a4b45673fc86e5ca1ad5d9dd33c05
Raheem Kawojue <kawojue08@gmail.com>
2023-01-01 03:54:48 +0100

    [edit] actions

--- src/reducer.js
+++ src/reducer.js
@@ -1,50 +1,50 @@
 const reducer = (state, action) => {
-    if (action.type === "CN-ERR") {
-        return {
-            ...state,
-            msg: true,
-            msgContent: 'Card Number must be 16 Digits'
+    switch (action.type) {
+        case "CN-ERR": {
+            return {
+                ...state,
+                msg: true,
+                msgContent: 'Card Number must be 16 Digits'
+            }
         }
-    }
-
-    if (action.type === "CHN-ERR") {
-        return {
-            ...state,
-            msg: true,
-            msgContent: 'Name too short'
+        case "CHN-ERR": {
+            return {
+                ...state,
+                msg: true,
+                msgContent: 'Name too short'
+            }
         }
-    }
-
-    if (action.type === "CARD-EXPR") {
-        return {
-            ...state,
-            msg: true,
-            msgContent: 'Card Expired',
+        case "CARD-EXPR": {
+            return {
+                ...state,
+                msg: true,
+                msgContent: 'Card Expired',
+            }
         }
-    }
-
-    if (action.type === "EXPR-ERR") {
-        return {
-            ...state,
-            msg: true,
-            msgContent: 'Expiry Date is Invalid',
+        case "EXPR-ERR": {
+            return {
+                ...state,
+                msg: true,
+                msgContent: 'Expiry Date is Invalid',
+            }
         }
-    }
-
-    if (action.type === "PAY") {
-        return {
-            ...state,
-            info: action.payload,
-            msg: true,
-            msgContent: 'Checking more Validation...'
+        case "PAY": {
+            return {
+                ...state,
+                info: action.payload,
+                msg: true,
+                msgContent: 'Checking more Validation...'
+            }
         }
-    }
-
-    if (action.type === "CLOSE") {
-        return {
-            ...state,
-            msg: false,
-            msgContent: ''
+        case "CLOSE": {
+            return {
+                ...state,
+                msg: false,
+                msgContent: ''
+            }
+        }
+        default: {
+            return state
         }
     }
 }
